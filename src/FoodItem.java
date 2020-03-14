3ab04908b71a2459a9aea044b905658432569b66
PATNODEIA17 <PATNODEIA17@5CG73107TP.GCC.edu>
2020-03-14 17:46:33 -0400

    Created FoodItem class

---
+++ src/FoodItem.java
@@ -0,0 +1,60 @@
+/**
+ * Class that pertains to the food items
+ * @author Isabella Patnode
+ *
+ */
+public class FoodItem {
+	private String name; //name of the food item
+	private double weight; //weight of food item
+	
+	/**
+	 * Default constructor for FoodItem class
+	 */
+	public FoodItem() {
+		name = "";
+		weight = 0.0;
+	}
+	
+	/**
+	 * Copy constructor for FoodItem class
+	 * @param name the name of the food item
+	 * @param weight the weight of the food item
+	 */
+	public FoodItem(String name, double weight) {
+		this.name = name;
+		this.weight = weight;
+	}
+	
+	/**
+	 * Updates the name of the current food item
+	 * @param name the new name of the food item
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Updates the weight of the current food item
+	 * @param weight the new weight of the food item
+	 */
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	/**
+	 * Method to access the name of a food item
+	 * @return the name of the current food item
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Method to access the weight of a food item
+	 * @return the weight of the current food item
+	 */
+	public double getWeight() {
+		return weight;
+	}
+	
+}
