6c974f96ef385e00ebfabf974554b9a2104a2621
PATNODEIA17 <PATNODEIA17@5CG73107TP.GCC.edu>
2020-03-15 21:13:23 -0400

    The start of the order class

---
+++ src/Order.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Order {
+	private ArrayList<Meal> orderList;
+	//private DeliveryPoint destination;
+	long timeOrdered;
+	long timeDelivered;
+	
+	public Order() {
+		orderList = new ArrayList<Meal>();
+		//timeOrdered equal current time
+	}
+	
+
+}
