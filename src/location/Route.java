4b1e53d9be4c106b96b218a8f100e70b08e1e866
RBOrtmann <ortmannrb17@gcc.edu>
2020-04-02 20:59:01 -0400

    Began implementation of Simulated Annealing for TSP

--- src/location/Route.java
+++ src/location/Route.java
@@ -1,57 +1,58 @@
 package location;
 
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Random;
 
 /**
-* Class containing the "route" the drone will take as a queue of Points. Also tracks distance traveled.
+* Class containing the "route" the drone will take as a queue of Points.
 * @author Brendan Ortmann
 */
 
-//Taking in LL<Orders> and constructing LL<Points>: route
 public class Route {
-    private LinkedList<Point> points;
-    private double distanceTraveled;
-    private int currPoint;
-    private Iterator<Point> pointIterator;
     private LinkedList<Point> route;
 
-    public Route(LinkedList<Point> points){
-        this.points = new LinkedList<>(points);
-        pointIterator = points.iterator();
-        distanceTraveled = 0;
-        currPoint = 0;
-        route = new LinkedList<>();
+    public Route(LinkedList<Point> points){ // Needs error checking
+        route = new LinkedList<>(points);
     }
 
-    /**
-     * Returns the distance traveled along the route so far.
-     * @return the double distanceTraveled
-     */
-    public double getDistanceTraveled(){
-        return distanceTraveled;
-    }
-
-    /**
-     * Returns the next destination on the Route or null if there are none remaining.
-     * @return the next {@link Point} in the queue
-     */
-    public Point getNextPoint(){
-        if(!pointIterator.hasNext())
-            return null;
-
-        Point p = pointIterator.next();
-        currPoint++;
-        return p;
-    }
-
-    // TODO: greedy or recursive backtracking/DFS for traveling salesman?
-
+    // Simulated Annealing from Baeldung
     public LinkedList<Point> getRoute() {
+        double t = 100; // Starting temperature
+        int numIterations = 10000; // Number of iterations before stopping
+        double bestDistance = getDistance();
+        Random r = new Random();
 
-        // Iterate through Orders to get Destinations
-        // Construct route based on destinations
+        for(int i = 0; i < numIterations; i++){
+            if(t < 0.1)
+                break;
+
+            int p = r.nextInt(route.size()), q = r.nextInt(route.size());
+            swapPoints(p, q); // Swap two random indices in route
+
+            double distance = getDistance();
+            if(distance < bestDistance)
+                bestDistance = distance;
+            else if(Math.exp((bestDistance - distance) / t) < Math.random())
+                swapPoints(q, p); // SA allows for "bad" trades under the above criterion: if false, reverse the swap
+
+            t = (t / Math.log(numIterations)); // "Cooling" function to lower temperature iteratively
+        }
 
         return route;
+    }
+
+    public void swapPoints(int x, int y){
+        Collections.swap(route, x, y);
+    }
+
+    public double getDistance(){
+        double d = 0;
+        Point previousPoint = null;
+        for (Point point : route) {
+            d += point.distanceFromPoint(previousPoint);
+            previousPoint = point;
+        }
+        return d;
     }
 }
