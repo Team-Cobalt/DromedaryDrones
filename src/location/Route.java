602391a2cdffb0ca9fbbf539d926254174545ab8
Brendan Ortmann <ortmannrb17@gcc.edu>
2020-04-05 16:04:11 -0400

    Implemented backtracking for TSP.
    Also generalized some methods in Route to operate on whatever LinkedList of Points is passed to them instead of just the route member variable.

--- src/location/Route.java
+++ src/location/Route.java
@@ -5,22 +5,57 @@ import java.util.LinkedList;
 import java.util.Random;
 
 /**
-* Class containing the "route" the drone will take as a queue of Points.
+* Class containing the "route" the drone will take as a queue of {@link Point}s.
 * @author Brendan Ortmann
 */
+
+// TODO: Error checking in all functions
 
 public class Route {
     private LinkedList<Point> route;
 
-    public Route(LinkedList<Point> points){ // Needs error checking
+    public Route(LinkedList<Point> points){
         route = new LinkedList<>(points);
+        route = calculateRouteDFS(route, 0, 0, getDistance(route));
+        // Calculate the optimal route using DFS w/pruning
+    }
+
+    /**
+     * Function that calculates the minimum weight Hamiltonian cycle for the given list of {@link Point}s using recursive
+     * backtracking and alpha-beta pruning. Adapted from {@see https://www.win.tue.nl/~kbuchin/teaching/2IL15/backtracking.pdf}.
+     * @author Brendan Ortmann
+     * @param currentRoute the list of {@code Point}s
+     * @param index the current index being looked at in the current cycle construction
+     * @param distanceSoFar the distance so far in the current cycle construction
+     * @param bestDistance the smallest distance we've seen in any cycle so far
+     * @return {@code LinkedList} of {@code Point}s which gives the shortest possible distance when traversed in order
+     */
+    public LinkedList<Point> calculateRouteDFS(LinkedList<Point> currentRoute, int index, double distanceSoFar,
+                                               double bestDistance){
+        int n = currentRoute.size();
+
+        if(index == n)
+            bestDistance = Math.min(bestDistance, distanceSoFar + currentRoute.get(n-1).distanceFromPoint(null));
+
+        for(int i = index + 1; i <= n; i++){
+            swapPoints(index+1, i, currentRoute);
+            double newLength = distanceSoFar + currentRoute.get(index).distanceFromPoint(currentRoute.get(index+1));
+            if(newLength >= bestDistance)
+                continue;
+            bestDistance = Math.min(bestDistance, getDistance(calculateRouteDFS(currentRoute, index + 1,
+                    newLength, bestDistance)));
+            swapPoints(i, index+1, currentRoute);
+        }
+
+        return currentRoute;
     }
 
     // Simulated Annealing from Baeldung
-    public LinkedList<Point> getRoute() {
+    // TODO: IGNORE FOR NOW
+    public LinkedList<Point> calculateRouteSA() {
         double t = 100; // Starting temperature
         int numIterations = 10000; // Number of iterations before stopping
-        double bestDistance = getDistance();
+        double bestDistance = getDistance(route);
         Random r = new Random();
 
         for(int i = 0; i < numIterations; i++){
@@ -28,13 +63,13 @@ public class Route {
                 break;
 
             int p = r.nextInt(route.size()), q = r.nextInt(route.size());
-            swapPoints(p, q); // Swap two random indices in route
+            swapPoints(p, q, route); // Swap two random indices in route
 
-            double distance = getDistance();
+            double distance = getDistance(route);
             if(distance < bestDistance)
                 bestDistance = distance;
             else if(Math.exp((bestDistance - distance) / t) < Math.random())
-                swapPoints(q, p); // SA allows for "bad" trades under the above criterion: if false, reverse the swap
+                swapPoints(q, p, route); // SA allows for "bad" trades under the above criterion: if false, reverse the swap
 
             t = (t / Math.log(numIterations)); // "Cooling" function to lower temperature iteratively
         }
@@ -42,17 +77,41 @@ public class Route {
         return route;
     }
 
-    public void swapPoints(int x, int y){
-        Collections.swap(route, x, y);
+    /**
+     * Swaps two Points in the given collection.
+     * @param x the index of the first {@link Point}
+     * @param y the index of the second {@code Point}
+     * @param points the collection in which {@code Point}s are swapped
+     */
+    public void swapPoints(int x, int y, LinkedList<Point> points){
+        Collections.swap(points, x, y);
     }
 
-    public double getDistance(){
+    /**
+     * Function that returns the total distance when traversing the given list of points in order. Since the list does
+     * not include the starting {@link Point}, {@code null} is used to calculate the distance between the given {@code Point} and the starting
+     * {@code Point} at the beginning of the path and the end.
+     * @param points the list of {@code Point}s for which we are calculating the distance
+     * @return the total distance when traversing the {@code Point}s in order
+     */
+    public double getDistance(LinkedList<Point> points){
         double d = 0;
         Point previousPoint = null;
-        for (Point point : route) {
+        for (Point point : points) {
             d += point.distanceFromPoint(previousPoint);
             previousPoint = point;
         }
+
+        d += points.get(points.size()-1).distanceFromPoint(null); // Add distance from last point to the start
+
         return d;
+    }
+
+    /**
+     * Getter for route member variable.
+     * @return {@code route} member variable
+     */
+    public LinkedList<Point> getRoute(){
+        return route;
     }
 }
