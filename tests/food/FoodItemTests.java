0aa391de2f9d1e10d54230975f9bba3314486e4f
Brendan Ortmann <ortmannrb17@gcc.edu>
2020-04-08 22:09:26 -0400

    Updated foodItemTests to FoodItemTests

--- tests/food/FoodItemTests.java
+++ tests/food/FoodItemTests.java
@@ -3,7 +3,7 @@ package food;
 /**
  * @author Brendan Ortmann
  */
-public class foodItemTests {
+public class FoodItemTests {
     /*
      * This is where the unit tests for the foodItem class will go
      */
