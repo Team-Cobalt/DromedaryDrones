1518147df17b989f8c8d7791bd63c072b3160dde
Brendan Ortmann <ortmannrb17@gcc.edu>
2020-04-25 15:00:27 -0400

    Some things

--- tests/com/dromedarydrones/food/FoodItemTests.java
+++ tests/com/dromedarydrones/food/FoodItemTests.java
@@ -1,16 +1,33 @@
 package com.dromedarydrones.food;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * @author Brendan Ortmann
  */
 public class FoodItemTests {
-    /*
-     * This is where the unit tests for the foodItem class will go
+
+    /**
+     *
      */
     @Test
-    public void shouldFail(){
-        System.out.println("This should fail.");
+    public void checkDefaultConstructor(){
+        FoodItem food = new FoodItem();
+        Assert.assertEquals("Incorrect default name.", "", food.getName());
+        Assert.assertEquals("Incorrect default weight.", 0.0, food.getWeight(), 0);
     }
+
+    @Test
+    public void checkParamConstructor(){
+        String name = "Large Pizza";
+        double weight = 96.0;
+        FoodItem food = new FoodItem(name, weight);
+
+        Assert.assertEquals("Incorrect parameterized name.", name, food.getName());
+        Assert.assertEquals("Incorrect parameterized weight.", weight, food.getWeight(), 0);
+    }
+
+
+
 }
